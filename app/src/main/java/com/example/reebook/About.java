fb6db6ca2c51b975d105c3ce289ef15a895ac2b5
mostafizur06 <mostafizur1042@gmail.com>
2023-11-19 01:47:41 +0600

    first commit

---
+++ app/src/main/java/com/example/reebook/About.java
@@ -0,0 +1,57 @@
+package com.example.reebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class About extends AppCompatActivity {
+
+    CardView instagram,facebook,twitter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        instagram = findViewById(R.id.instagram);
+        facebook = findViewById(R.id.facebook);
+        twitter = findViewById(R.id.twitter);
+
+        instagram.setOnClickListener(new View.OnClickListener ()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent myWebLink = new Intent(android.content.Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("http://www.instagram.com/mostafizur_sohan"));
+                startActivity(myWebLink);
+            }
+        });
+        facebook.setOnClickListener(new View.OnClickListener ()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent myWebLink = new Intent(android.content.Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("http://www.facebook.com/sohan.mostafizur"));
+                startActivity(myWebLink);
+            }
+        });
+        twitter.setOnClickListener(new View.OnClickListener ()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent myWebLink = new Intent(android.content.Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("http://www.twitter.com"));
+                startActivity(myWebLink);
+            }
+        });
+    }
+
+}
